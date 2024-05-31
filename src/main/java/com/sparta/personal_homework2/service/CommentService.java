ce5cc2e12921e9a0bb1dbdded4d06cb442fb076f
erictslee <ts5452@naver.com>
2024-05-31 16:03:32 +0900

    4단계 - 댓글 삭제

--- src/main/java/com/sparta/personal_homework2/service/CommentService.java
+++ src/main/java/com/sparta/personal_homework2/service/CommentService.java
@@ -51,5 +51,21 @@ public class CommentService {
         return CommentResponse.toDto(comment);
     }
 
+    public void delete(Long scheduleId, Long commentId, String username){
+
+        scheduleRepository.findById(scheduleId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 id에 맞는 일정 데이터가 없습니다. 아이디 : " + scheduleId));
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(()-> new DataNotFoundException("해당 댓글이 DB에 존재하지 않습니다."));
+
+        if(!Objects.equals(comment.getUsername(), username)) {
+            throw new IllegalArgumentException("사용자가 일치하지 않습니다.");
+        }
+
+        commentRepository.delete(comment);
+
+    }
+
     }
 }
